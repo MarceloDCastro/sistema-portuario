49ffc1b8c67006e30e2f8923c3884a8255048eba
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2021-05-05 11:28:22 -0300

    First commit

---
+++ demo/src/main/java/com/t2s/demo/SistemaPortuarioApplication.java
@@ -0,0 +1,20 @@
+package com.t2s.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SistemaPortuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPortuarioApplication.class, args);
+		System.out.println("Ola Mundooooooo!");
+	}
+	
+	@GetMapping("/")
+	  public String hello() {
+	    return "Olá mundo!";
+	  }
+
+}
